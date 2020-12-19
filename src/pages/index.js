13b096ce7f0324ea8de8c77213616e4258a6e554
bajinho <bajo@live.jp>
2020-12-19 00:44:21 -0300

    changed layout homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Flex, Heading, Spacer, Button } from '@chakra-ui/react';
 import { Link } from 'gatsby';
 
 function IndexPage() {
   return (
-    <Box p={8}>
-      <Link to="/pokemon">
-        <Text>My Pokémon</Text>
-      </Link>
-    </Box>
+    <Flex>
+      <Box p="2">
+        <Heading size="md">
+          <Link to="/pokemon">
+            <Text>My Pokémon</Text>
+          </Link>
+        </Heading>
+      </Box>
+      <Spacer />
+      <Box>
+        <Box as="button" borderRadius="md" bg="tomato" px={4} h={10}>
+          <Link to="/pokemon">
+            <Text>My Pokémon</Text>
+          </Link>
+        </Box>
+      </Box>
+    </Flex>
   );
 }
 
